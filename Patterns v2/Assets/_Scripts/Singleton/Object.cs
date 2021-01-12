fd41fa1a2b8c20a1387d1d4c5d510b8652e84dd8
CdrcMag <cdrcmag@outlook.fr>
2021-01-12 10:00:45 +0100

    Ajout singleton

---
+++ Patterns v2/Assets/_Scripts/Singleton/Object.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour, IObject
+{
+    public string type;
+
+    public void Type()
+    {
+        switch(type)
+        {
+            case "Sphere": ObjectTracker.Instance.setTextToSphere(); break;
+            case "Cube": ObjectTracker.Instance.setTextToCube(); break;
+            case "Cylindre": ObjectTracker.Instance.setTextToCylindre(); break;
+            default: break;
+        }
+    }
+
+
+    private void OnMouseDown() => Type();
+
+
+}
---
+++ Patterns v2/Assets/_Scripts/Singleton/ObjectTracker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectTracker : MonoBehaviour
+{
+    private static ObjectTracker _instance;
+    public static ObjectTracker Instance
+    {
+        get
+        {
+            if (!_instance)
+                Debug.LogError("The object tracker is missing.");
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public TextMeshProUGUI mainText;
+
+
+    public void setTextToCube() => mainText.text = "Vous avez cliqué sur un cube.";
+    public void setTextToCylindre() => mainText.text = "Vous avez cliqué sur un cylindre.";
+    public void setTextToSphere() => mainText.text = "Vous avez cliqué sur une sphere.";
+
+
+
+}
